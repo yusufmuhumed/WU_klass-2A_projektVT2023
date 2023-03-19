1670c5bd4d4697557206352702e3775109d615bc
yusufmuhumed <yusuf.muhumed@elev.ga.ntig.se>
2023-03-19 23:09:47 +0100

    jobb

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const header = document.querySelector(".page-header");
 const toggleClass = "is-sticky";
 window.addEventListener("scroll", () => {
   const currentScroll = window.pageYOffset;
-  if (currentScroll > 150) {
+  if (currentScroll > 125) {
     header.classList.add(toggleClass);
   } else {
     header.classList.remove(toggleClass);
